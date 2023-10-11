8afab67c883adf088ec75f3c0746d51c3d161581
Sara Alaqeel <sara.f.alaqeel@gmail.com>
2023-10-11 17:05:07 +0300

    final

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkprime(num):
+
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+
+    else:
+        return False
+
 def primes(number_of_primes):
     list = []
+    num =2
+    if number_of_primes <= 0:
+        raise ValueError("The number must be a positive integer greater than zero")
+
+    while len(list) < number_of_primes:
+        if(checkprime(num)):
+           list.append(num)
+        num = num +1
     return list
